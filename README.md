b9ba88c6687e604e427d5103f0e5e44dc8528e4e
Gary Estime <visionzbg@yahoo.com>
2018-04-18 22:58:37 -0400

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # friend_f_homework
+
+https://visionzbg.github.io/friend_f_homework/
+
+https://github.com/visionzbg/friend_f_homework
--- app/routing/api_routes.js
+++ app/routing/api_routes.js
@@ -1,6 +1,6 @@
 //a POST routes /api/friends - this handles incoming survey results. will also used to handle the compatibility logic
 //Load Data
-var friendList = require('../data/friend.js');
+var friendList = require('../data/friends_list.js');
 
 module.exports = function(app){
   //a GET route that displays JSON of all possible friends
