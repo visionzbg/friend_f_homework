4a72f6404eeb52d659b8d6529646a5135879905a
Gary Estime <visionzbg@yahoo.com>
2018-04-18 22:38:09 -0400

    Creation on HW folder

---
+++ app/data/friends_list.js
@@ -0,0 +1,22 @@
+//Array of friends
+
+var friendList = [
+    {
+      name: 'Gary Estime',
+      profilePic: 'https://.........',
+      "scores":[
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ];
+  module.exports = friendList;
+  
---
+++ app/routing/html_routes.js
@@ -0,0 +1,17 @@
+//Dependencies
+var path = require('path');
+
+//ROUTING
+
+module.exports = function(app){
+
+// Route to Survey (HTML) page
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+  });
+
+  //Route to Home  (HTML) page
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/index.html'));
+  });
+};
